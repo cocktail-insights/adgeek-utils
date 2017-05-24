ce6e481c53f05fe82ed3bdeda2e9ef124f520571
Samuel Amoah <snamoah@gmail.com>
2017-05-24 09:41:00 +0000

    remove trialing symbols in hyphenated string

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -39,3 +39,12 @@ test('hyphenate returns lowercased string with hyphens', (t) => {
   t.equal(newStr, 'mr-and-mrs-foo-bar');
   t.end();
 });
+
+test('hyphenate returns lowercased string without trailing hyphens', (t) => {
+  const str = "The string, that brings: both 1 + 2 = 3; together & decypher's code is brave! (@menacle).     ";
+
+  const newStr = Utils.hyphenate(str);
+
+  t.equal(newStr, 'the-string-that-brings-both-1-2-3-together-decyphers-code-is-brave-menacle');
+  t.end();
+});
